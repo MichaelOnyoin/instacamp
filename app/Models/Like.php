d709295c18f48b0f6c756af42fff1a5d900b6c41
MichaelOnyoin <moonyoin@gmail.com>
2025-04-11 11:38:18 +0300

    1st commit

---
+++ app/Models/Like.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Like extends Model
+{
+    //
+    protected $connection = 'mongodb';
+    protected $table = 'likes';
+
+    protected $fillable = [
+        'user_id',
+        'post_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function post():BelongsTo
+    {
+        return $this->belongsTo(Post::class);
+    }
+
+    public function scopeWithPost($query, $postId)
+    {
+        return $query->where('post_id', $postId);
+    }
+
+}
---
+++ app/Models/Post.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    //
+    protected $connection = 'mongodb';
+    protected $table = 'posts';
+
+    protected $fillable = [
+        'caption',
+        'image_path',
+        'image_url',
+        'user_id',
+        
+    ];
+
+    public function user():BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function likes(): HasMany
+    {
+        return $this->hasMany(Like::class);
+    }
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
+    }
+}
