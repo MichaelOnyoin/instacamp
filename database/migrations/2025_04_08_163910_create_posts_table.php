2c85780cd2554705773c4239804150381902ea1a
Michael Onyoin <moonyoin@gmail.com>
2025-04-30 14:57:16 +0300

    Update 2025_04_08_163910_create_posts_table.php

--- database/migrations/2025_04_08_163910_create_posts_table.php
+++ database/migrations/2025_04_08_163910_create_posts_table.php
@@ -12,10 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('posts', function (Blueprint $table) {
-            $table->id('post_id');
-            $table->string('caption');
-            $table->string('image_path')->nullable();
-            $table->string('video_path')->nullable();
+            $table->id();
             $table->timestamps();
         });
     }
