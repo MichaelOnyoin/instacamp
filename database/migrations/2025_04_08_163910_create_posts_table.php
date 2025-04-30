2724cc7b0a3e8ba121f5a5b86fdc1f39e13bbb04
Michael Onyoin <moonyoin@gmail.com>
2025-04-30 14:52:13 +0300

    Update 2025_04_08_163910_create_posts_table.php

--- database/migrations/2025_04_08_163910_create_posts_table.php
+++ database/migrations/2025_04_08_163910_create_posts_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->id('post_id');
             $table->string('caption');
             $table->string('image_path')->nullable();
-            $table->id('user_id');
             $table->string('video_path')->nullable();
             $table->timestamps();
         });
